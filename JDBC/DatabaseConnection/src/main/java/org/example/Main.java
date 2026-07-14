1b06dce78455cf2c0e8f303c036949ace955185e
somjena <jenasoumitrapvt@gmail.com>
2026-07-14 11:23:56 +0530

    Update in main method

--- JDBC/DatabaseConnection/src/main/java/org/example/Main.java
+++ JDBC/DatabaseConnection/src/main/java/org/example/Main.java
@@ -1,17 +1,28 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String url = "jdbc:mysql://localhost:3306/student";
+        String user = "root";
+        String password = "Rintu@1234";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            Statement stmt=connection.createStatement();
+           int rows = stmt.executeUpdate("insert into student_data values (54,'ajit','Mernstack')");
+            System.out.println(rows);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+
+
     }
-}
+
+    }
